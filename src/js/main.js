8e6c756ed08f85376c6a1c53b6bcc65057297871
Łukasz Mijała <lukasz.mijala@gmail.com>
2022-05-24 00:26:07 +0200

    added counter with localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -29,18 +29,33 @@ const el = document.createElement("span");
 
 let counter = 0;
 
+window.onload = () => {
+  counter = localStorage.getItem("counter")
+    ? JSON.parse(localStorage.getItem("counter"))
+    : 0;
+  infoClick.innerText = counter;
+};
+
 function elementClick() {
-    counter++;
-    infoClick.innerText = `${counter} times`;
-    if (counter >= 5) {
-        btn.removeEventListener("click", elementClick)
-    } if (counter == 5) {
-      el.classList.add("element");
-      el.innerText = `Reset Now!`;
+  counter = counter+1;   
+  infoClick.innerText = counter + ` times`;
+  localStorage.setItem("counter", JSON.stringify(counter));
 
-      const span = document.querySelector(".btn-reset");
-      span.appendChild(el);
-    }
+  if (counter == 5) {
+    btn.removeEventListener("click", elementClick);  
+
+    el.classList.add("element");
+    el.innerText = `Reset Now!`;
+    const span = document.querySelector(".btn-reset");
+    span.appendChild(el);
+  }    
 }
-
 btn.addEventListener("click", elementClick);
+
+
+const btnReset = document.querySelector(".btn-reset");
+
+function localStorageClickClear() {
+  localStorage.clear('counter'); 
+}
+btnReset.addEventListener("click", localStorageClickClear);
